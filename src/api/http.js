cf677480f5aded716874b6dc904e50f83ecb2fd1
asadahimeka <smallstar9637@yahoo.co.jp>
2022-08-11 11:05:42 +0800

    change api

--- src/api/http.js
+++ src/api/http.js
@@ -2,13 +2,20 @@ import axios from 'axios'
 
 const baseURL = 'https://hibiapi.getloli.com/'
 
-axios.defaults.baseURL = baseURL
+// axios.defaults.baseURL = baseURL
 axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 const get = async (url, params) => {
   try {
-    const res = await axios.get(url, { params })
+    const api = new URL('https://kw.kanata.ml')
+    const req_url = new URL(url, baseURL)
+    api.searchParams.set('_vercel_no_cache', '1')
+    Object.keys(params).forEach(k => {
+      req_url.searchParams.set(k, params[k])
+    })
+    api.searchParams.set('u', req_url.href)
+    const res = await axios.get(api.href)
 
     return new Promise((resolve, reject) => {
       let data = res.data
