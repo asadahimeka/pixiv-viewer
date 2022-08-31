638b6f4c0196c5ce0ca9c106e57bf7e6f0c7f331
asadahimeka <smallstar9637@yahoo.co.jp>
2022-08-31 10:30:38 +0800

    change api url

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'https://hibiapi.getloli.com/api'
+const baseURL = 'https://api.imki.moe/api'
 
 axios.defaults.baseURL = baseURL
 axios.defaults.timeout = 10000
