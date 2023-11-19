2cd4b3bc2baf080439d8910bf08ffa493901c29b
asadahimeka <smallstar9637@yahoo.co.jp>
2023-11-19 18:05:07 +0800

    fix: move hide rank manga to content setting

--- src/views/Setting/OtherSetting.vue
+++ src/views/Setting/OtherSetting.vue
@@ -25,11 +25,6 @@
         <van-switch :value="isLongpressDL" size="24" @change="changeLongpressDL" />
       </template>
     </van-cell>
-    <van-cell center :title="$t('3HnNTIScyvd1cNc2qAh7X')" :label="$t('qmd5JADeSGtrvucK3TnGb')">
-      <template #right-icon>
-        <van-switch v-model="isHideRankManga" size="24" @change="changeHideRankManga" />
-      </template>
-    </van-cell>
     <van-cell center :title="$t('setting.other.manual_input')" :label="$t('setting.other.manual_input_label')">
       <template #right-icon>
         <van-switch v-model="hideApSelect" size="24" />
@@ -229,7 +224,6 @@ export default {
       pximgChecked: true,
       apiChecked: true,
       hideApSelect: LocalStorage.get('__HIDE_AP_SEL', false),
-      isHideRankManga: LocalStorage.get('PXV_HIDE_RANK_MANGA', false),
       isDark: !!localStorage.getItem('PXV_DARK'),
       enableSwipe: LocalStorage.get('PXV_IMG_DTL_SWIPE', false),
       isPageEffectOn: LocalStorage.get('PXV_PAGE_EFFECT', false),
@@ -425,15 +419,6 @@ export default {
       this.isLongpressDL = val
       this.$nextTick(() => {
         LocalStorage.set('PXV_LONGPRESS_DL', val)
-        setTimeout(() => {
-          location.reload()
-        }, 500)
-      })
-    },
-    changeHideRankManga(val) {
-      this.isHideRankManga = val
-      this.$nextTick(() => {
-        LocalStorage.set('PXV_HIDE_RANK_MANGA', val)
         setTimeout(() => {
           location.reload()
         }, 500)
