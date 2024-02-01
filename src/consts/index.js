f592e1e8e5933e8f99f760c216809e411d2becf6
asadahimeka <smallstar9637@yahoo.co.jp>
2024-02-01 23:09:46 +0800

    fix: update env

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,6 +1,6 @@
 import { LocalStorage } from '@/utils/storage'
 
-export const DEF_HIBIAPI_MAIN = process.env.VUE_APP_DEF_HIBIAPI_MAIN
+export const DEF_HIBIAPI_MAIN = process.env.VUE_APP_DEF_HIBIAPI_SFW
 export const DEF_PXIMG_MAIN = process.env.VUE_APP_DEF_PXIMG_MAIN
 export const DEF_API_PROXY = process.env.VUE_APP_DEF_APP_API_PROXY
 export const PXIMG_PROXYS = process.env.VUE_APP_PXIMG_PROXYS || ''
